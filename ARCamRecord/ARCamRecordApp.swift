d1636ca636136468a053793783cb71c8fc4cc20b
Alexander <vvruspat@gmail.com>
2023-07-28 10:08:05 +0000

    Initial Commit

---
+++ ARCamRecord/ARCamRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARCamRecordApp.swift
+//  ARCamRecord
+//
+//  Created by Елена Белова on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ARCamRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
